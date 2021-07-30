ad610c7b357cbe81faf0726b4ffdea4df2356298
Nelson Silva <nelson.jf.silva@gmail.com>
2021-07-30 14:00:15 +0100

    Leveler

---
+++ MicLeveler/Program.cs
@@ -0,0 +1,26 @@
+﻿using CoreAudioApi;
+using System.Threading;
+
+namespace MicLeveler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+
+            var device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eMultimedia);
+
+            while (true) 
+            {
+                if (device.AudioEndpointVolume.MasterVolumeLevel != 0)
+                {
+                    device.AudioEndpointVolume.MasterVolumeLevel = 0;
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
